d32796c9a70521be3825a376e96662042f719fe6
Hyundai <Hyundai@DESKTOP-75OGHNO>
2015-10-19 19:24:24 +0200

    tester scada

--- src/newpackage/Main.java
+++ src/newpackage/Main.java
@@ -5,5 +5,6 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Dette er vores SCADA");
+        System.out.println("Tester lige en gang til");
     }    
 }
