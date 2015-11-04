5f997c60eba677caebb095e1d3431bc46d9e3037
Knudsen <Knudsen@DESKTOP-21INLDK>
2015-11-04 21:21:37 +0100

    Distribueret

--- src/wsservers/BatchServer.java
+++ src/wsservers/BatchServer.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 public class BatchServer
 {
     public static final int PORT = 8003;
-    public static final String NAME = "BatchController";
+    public static final String NAME = "Batch";
     public static final String WSURL = "http://localhost:" + PORT + "/ws/" + NAME;
     
     public static void main(String[] args)
